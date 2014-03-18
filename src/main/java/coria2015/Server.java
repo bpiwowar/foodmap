92a373b5880dc8a51e27304551275aaa3830ba3e
Benjamin Piwowarski <benjamin@bpiwowar.net>
2014-03-18 14:48:00 +0100

    Added basic files

---
+++ src/main/java/coria2015/Server.java
@@ -0,0 +1,16 @@
+package coria2015;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.logging.Logger;
+
+/**
+ * Created by bpiwowar on 18/3/14.
+ */
+public class Server {
+    final static Logger LOGGER = Logger.getLogger("server");
+
+    static int main(String [] args) {
+    }
+}
