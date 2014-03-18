e3198cf363ac0ea909c8a964061c968b674b2169
Karen Sauvagnat <karen.pinel@gmail.com>
2014-03-19 00:26:10 +0100

    no message

--- src/main/java/coria2015/geoencoding/WikiFile.java
+++ src/main/java/coria2015/geoencoding/WikiFile.java
@@ -4,4 +4,14 @@ package coria2015.geoencoding;
  * Created by sauvagna on 18/03/14.
  */
 public class WikiFile {
+
+    public String file;
+    public String start_offset;
+    public String end_offset;
+
+    WikiFile(String file, String start_offset, String end_offset){
+        this.file=file;
+        this.start_offset=start_offset;
+        this.end_offset=end_offset;
+    }
 }
